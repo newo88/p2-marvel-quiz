b7e192ee0bec27b3c1fb2b45054b40d611778ee8
Owen McGill <owen.mcgill88@gmail.com>
2021-06-22 21:29:34 +0000

    Code added  to add random questions and display to screen

--- assets/js/ironman.js
+++ assets/js/ironman.js
@@ -1,1 +1,22 @@
-console.log("connected")
+ let ironmanQuestions = [
+     {
+         question: "whats iron mans alter ego?",
+         choiceA: "Tony stark",
+         choiceB: " addd",
+         choiceC: "aaaaaaa",
+         choiceD: "hhhhhhhh",
+
+         correct: choiceA
+
+     },
+
+     {
+        question: "whats 2 + 2",
+         choiceA: "Tony stark",
+         choiceB: " addd",
+         choiceC: "aaaaaaa",
+         choiceD: "hhhhhhhh",
+
+         correct: choiceB,
+     }
+ ]
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,2 +1,33 @@
-let answer = 2*2
-console.log(answer)
+const choiceA = document.getElementById("choiceA")
+const choiceB = document.getElementById("choiceB")
+const choiceC = document.getElementById("choiceC")
+const choiceD = document.getElementById("choiceD")
+const questionElement = document.getElementById("questions");
+let answersElements = document.getElementsByClassName("user-choice");
+let randomIndex = Math.floor(Math.random() * ironmanQuestions.length);
+
+//document.getElementById("choiceA").addEventListener("click");
+
+
+
+
+
+
+function renderQuestion(){
+   
+questionElement.innerHTML = ironmanQuestions[randomIndex].question;
+for (let i = 0; i < answersElements.length; i++) {
+    console.log(121) 
+//answerElements = answersElements[0].innerHTML;
+ 
+  choiceA.innerHTML = choiceA;
+  choiceB.innerHTML = choiceB;
+  choiceC.innerHTML = choiceC;
+  choiceD.innerHTML = choiceD;
+  console.log(choiceA)
+  
+}
+}
+
+renderQuestion()
+
