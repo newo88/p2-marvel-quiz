b4793cac0bda9bdb41fd555826732989bc530d77
Owen McGill <owen.mcgill88@gmail.com>
2021-06-29 17:45:35 +0000

    Added code for buttons to select which quiz user would like to try

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,45 +1,50 @@
 const questions = document.getElementById('questions');
 //Array.from() method returns an Array object from any object with a length property or an iterable object.
 const choices = Array.from(document.getElementsByClassName('choice-text')); 
-//const correctIncorrect = document.getElementById("correct-incorrect");
 let currentQuestion = {};
-let acceptingAnswer = true;
+//let acceptingAnswer = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let MAX_QUESTIONS = 5;
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
-
-    for(let button of buttons){
+;
+document.addEventListener("DOMContentLoaded", function (){
+    let buttons = document.getElementsByTagName("button")
+    for (let button of buttons){
         button.addEventListener("click", function(){
-            if(this.getAttribute("data-type") === "ironman"){
-                return window.location.assign("quiz.html");
-                startGame();
-            }else {
-                let gameType = this.getAttribute("data-type")
-                alert(`Unknow game ${gameType}`)
-            }
-        })
+           if(this.getAttribute("data-type") === "ironman"){
+               startGameIron();
+           }else {this.getAttribute("data-type") === "thor" 
+           startGameThor();}
+
+        });
+
     }
 })
 
 
 
+function startGameIron() {
+      questionCounter = 0;
+      score = 0;
+      availableQuesions = [...ironmanQuiz];
+      getNewQuestion();
 
-function startGame() {
+}
+
+function startGameThor() {
     questionCounter = 0;
     score = 0;
-    availableQuesions = [...ironmanQuiz];
+    availableQuesions = [...thorQuiz];
     getNewQuestion();
+
 }
 
 
-
 function getNewQuestion() {
-if(availableQuestions.length === 5 || questionCounter >= MAX_QUESTIONS){
+if(availableQuesions.length === 10 || questionCounter >= MAX_QUESTIONS){
     return window.location.assign("end.html");
 }
     questionCounter++;
@@ -80,8 +85,7 @@ choices.forEach(choice =>{
 
 }
 
-
 checkAnswer();
 
-startGame();
+
 
---
+++ assets/js/thor.js
@@ -0,0 +1,122 @@
+const thorQuiz = [
+    
+    {
+        question: "asdfaf",
+        choice1: "Iron Man",
+        choice2: "Thor",
+        choice3: "Captain America",
+        choice4: "hulk",
+        answer: 1,
+         },
+
+     
+        {
+            question: "asdfaf" ,
+            choice1: "Chris Evens",
+            choice2: "Robert Downey Jr",
+            choice3: "Happy Hogan",
+            choice4: "Sandra ",
+            answer: 2,
+        },
+
+        {
+            question: "asdfaf",
+            choice1: "Antman",
+            choice2: "Winter Soldier",
+            choice3: "Spiderman",
+            choice4: "Black Widow",
+            answer: 3,
+        },
+
+        {
+            question: "asdfaf",
+            choice1: "2001",
+            choice2: "2014",
+            choice3: "20010",
+            choice4: "2008",
+            answer: 4,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "Iron Man",
+            choice2: "Super Pots",
+            choice3: "Rescue",
+            choice4: "Aqua Woman",
+            answer: 3,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "Monday",
+            choice2: "Haris",
+            choice3: "Marvin",
+            choice4: "Jarvis",
+            answer: 4,
+        },
+
+
+        {
+            question:"asdfaf",
+            choice1: "Iron Man",
+            choice2: "Robert Downey Jr",
+            choice3: "Captain America",
+            choice4: "hulk",
+            answer: 2,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "The Dark Elves",
+            choice2: "The Mandarin",
+            choice3: "The Mandalorian",
+            choice4: "The Silver Surfer",
+            answer: 2,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "Howard Stark",
+            choice2: "Batman",
+            choice3: "Captain America",
+            choice4: "Hulk",
+            answer: 3,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "Silver Heart",
+            choice2: "Curve Reactor",
+            choice3: "The Force",
+            choice4: "Arc Reactor",
+            answer: 4,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "The 10 Rings",
+            choice2: "Nick Fury",
+            choice3: "The Wakondians",
+            choice4: "Iron Mongler",
+            answer: 1,
+        },
+
+
+        {
+            question: "asdfaf",
+            choice1: "Plane Crash",
+            choice2: "Sailing",
+            choice3: "Car Crash",
+            choice4: "Storm",
+            answer: 3,
+        },
+
+
+ 
+]
