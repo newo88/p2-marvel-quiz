659faf61f3eb7ac19a82ac501da78088094a315c
Owen McGill <owen.mcgill88@gmail.com>
2021-07-03 12:18:01 +0000

    add code to hide quiz buttons and display questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,27 +2,37 @@ const questions = document.getElementById('questions');
 //Array.from() method returns an Array object from any object with a length property or an iterable object.
 const choices = Array.from(document.getElementsByClassName('choice-text')); 
 let currentQuestion = {};
-//let acceptingAnswer = true;
 let score = document.getElementById("score");
 let questionCounter = 0;
 let availableQuestions = [];
 let MAX_QUESTIONS = 5;
-let quizContainer = document.getElementsByClassName("quizselector")
+let buttons = document.getElementsByClassName("buttons")
 let correct = document.getElementById("correct");
-//questions.classList.add("hide")
-
+let gameArea = document.getElementById("gamearea")
+let answered = document.getElementById("answered");
+let startQuiz = document.getElementById("start-quiz")
+gameArea.style.display = "none";
+answered.style.display = "none";
 
 document.addEventListener("DOMContentLoaded", function (){
     let buttons = document.getElementsByTagName("button")
     for (let button of buttons){
         button.addEventListener("click", function(){
            if(this.getAttribute("data-type") === "ironman"){
-             //  quizContainer.classList.add('hide')
+               gameArea.style.display = "block"
+               startQuiz.style.display = "none"
                startGameIron();
+          
            }else if (this.getAttribute("data-type") === "thor"){ 
+            gameArea.style.display = "block"
+           
            startGameThor();
+          
         } else if (this.getAttribute("data-type") === "captainAmerica"){ 
+            gameArea.style.display = "block"
+           
             startGameAmerica();
+            
          } 
  
 
@@ -34,11 +44,13 @@ document.addEventListener("DOMContentLoaded", function (){
 
 
 function startGameIron() {
+   
       questionCounter = 0;
       score = 0;
       availableQuesions = [...ironmanQuiz];
       getNewQuestion();
       checkAnswer();
+      ;
 
 }
 
@@ -60,6 +72,8 @@ function startGameThor() {
 
 }
 
+
+
 /**
  * Increments the number of questions answered by the users.
  */
@@ -76,7 +90,14 @@ function correctAnswers(){
 
 function getNewQuestion() {
 if(availableQuesions.length === 10 || questionCounter >= MAX_QUESTIONS){
-    return window.location.assign("end.html");
+    //return window.location.assign("end.html");
+    answered.style.display = "block"
+    gameArea.style.display = "none";
+    setTimeout(() => {
+        return window.location.assign("index.html")
+        
+     }, 2000);
+    
 }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
