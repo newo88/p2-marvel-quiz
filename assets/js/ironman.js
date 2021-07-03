6bba279771693b0f0d6c5a0f05474408eb47321d
Owen McGill <owen.mcgill88@gmail.com>
2021-07-03 14:59:13 +0000

    add more questions to ironman.js

--- assets/js/ironman.js
+++ assets/js/ironman.js
@@ -117,6 +117,138 @@
                 answer: 3,
             },
 
+            {
+                question: "What was the super-name of Obidiah Stane?",
+                choice1: "Hawk Eye",
+                choice2: "Thor",
+                choice3: "Iron Mongler",
+                choice4: "Loki",
+                answer: 3,
+            },
+            
+            {
+                question: "What was the name of the peacekeeping AI Stark built?",
+                choice1: "P.E.A.C.E.",
+                choice2: "Veltron",
+                choice3: "Iron Shield",
+                choice4: "Ultron",
+                answer: 4,
+            },
+
+            {
+                question: "How did Tony's parents supposable die?",
+                choice1: "Plane Crash",
+                choice2: "Sailing",
+                choice3: "Car Crash",
+                choice4: "Storm",
+                answer: 3,
+            },
+
+            {
+                question: "What job does Happy Hogan do for Tony?",
+                choice1: "Butler",
+                choice2: "SideKick",
+                choice3: "Body Gaurd",
+                choice4: "Chauffeur",
+                answer: 4,
+            },
+
+            {
+                question: "What super-team is Iron Man a founding member of?",
+                choice1: "Iron Legion",
+                choice2: "Avengers",
+                choice3: "Team Titan",
+                choice4: "Gaurdians of the Galaxy",
+                answer: 2,
+            },
+
+            {
+                question: "What's Tony Stark's address?",
+                choice1: "Stark Industries",
+                choice2: "Asgaurd",
+                choice3: "10880 Malibu Point",
+                choice4: "New York",
+                answer: 3,
+            },
+
+            {
+                question: "What is the name of the suit Tony makes for Spider-Man?",
+                choice1: "Iron Spider",
+                choice2: "Silk Web",
+                choice3: "Spider Suit Mk2",
+                choice4: "Spidey Suit 2",
+                answer: 1,
+            },
+
+            {
+                question: "Who created the character of Iron Man? ",
+                choice1: "Stan Lee",
+                choice2: "Robert Lin",
+                choice3: "Stan Herbert",
+                choice4: "Stan W.S.",
+                answer: 1,
+            },
+
+            {
+                question: "What's the name of Tony Stark's father?",
+                choice1: "Tom",
+                choice2: "Harry",
+                choice3: "Francis",
+                choice4: "Howard",
+                answer: 4,
+            },
+
+            {
+                question: "Which college did Tony Stark go to?",
+                choice1: "Code Institute",
+                choice2: "Harverd",
+                choice3: "M.I.T.",
+                choice4: "Yale",
+                answer: 3,
+            },
+
+            {
+                question: "In which city is the F1 race that Tony takes part in?",
+                choice1: "Dublin",
+                choice2: "Paris",
+                choice3: "New York",
+                choice4: "Monico",
+                answer: 4,
+            },
+
+            {
+                question: "What Character killed Tony Starks Parents.?",
+                choice1: "Captain America",
+                choice2: "Winter Soldier",
+                choice3: "Thor",
+                choice4: "Iron Man",
+                answer: 2,
+            },
+
+            {
+                question: "Tony starks father created what Famous weapon.?",
+                choice1: "Captain America Shield",
+                choice2: "Iron Man",
+                choice3: "Twin Pulse lasers",
+                choice4: "Hulk Buster",
+                answer: 1,
+            },
+
+            {
+                question: "Whats the name of the suit tony used to fight the Hulk.?",
+                choice1: "Ultron",
+                choice2: "Hulk Buster",
+                choice3: "Jarvis",
+                choice4: "Rescue",
+                answer: 2,
+            },
+
+
+                 
+
+                 
+
+
 
      
  ]
