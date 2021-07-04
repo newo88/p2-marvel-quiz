17c3a047da15f97c18dfc99d91ca36ccf69afbd5
Owen McGill <owen.mcgill88@gmail.com>
2021-07-04 12:39:53 +0000

    Added all questions for thor and america quiz

--- assets/js/america.js
+++ assets/js/america.js
@@ -1,120 +1,192 @@
 const americaQuiz = [
     
     {
-        question: "2365214",
-        choice1: "Iron Man",
-        choice2: "Thor",
-        choice3: "Captain America",
-        choice4: "hulk",
-        answer: 1,
+        question: "What is Bucky's real first and middle name?",
+        choice1: "Brian Bucket",
+        choice2: "James Buchanan",
+        choice3: "Steve Rogers",
+        choice4: "Robert Buchwald",
+        answer: 2,
          },
 
      
         {
-            question: "2365214",
-            choice1: "Chris Evens",
-            choice2: "Robert Downey Jr",
-            choice3: "Happy Hogan",
-            choice4: "Sandra ",
-            answer: 2,
+            question: "What is the name of the evil organization within the Third Reich?",
+            choice1: "Hydra",
+            choice2: "The Seven Sons",
+            choice3: "Red Army",
+            choice4: "Iron Fist",
+            answer: 1,
         },
 
         {
-            question: "2365214",
-            choice1: "Antman",
-            choice2: "Winter Soldier",
-            choice3: "Spiderman",
-            choice4: "Black Widow",
+            question: "Whats Captain America first name?",
+            choice1: "Captain",
+            choice2: "Robert",
+            choice3: "Steve",
+            choice4: "Jack",
             answer: 3,
         },
 
         {
-            question: "2365214",
-            choice1: "2001",
-            choice2: "2014",
-            choice3: "20010",
-            choice4: "2008",
-            answer: 4,
-        },
-
-
-        {
-            question: "2365214",
-            choice1: "Iron Man",
-            choice2: "Super Pots",
-            choice3: "Rescue",
-            choice4: "Aqua Woman",
-            answer: 3,
-        },
-
-
-        {
-            question: "2365214",
-            choice1: "Monday",
-            choice2: "Haris",
-            choice3: "Marvin",
-            choice4: "Jarvis",
-            answer: 4,
-        },
-
-
-        {
-            question:"2365214",
-            choice1: "Iron Man",
-            choice2: "Robert Downey Jr",
-            choice3: "Captain America",
-            choice4: "hulk",
-            answer: 2,
-        },
-
-
-        {
-            question:"2365214",
-            choice1: "The Dark Elves",
-            choice2: "The Mandarin",
-            choice3: "The Mandalorian",
-            choice4: "The Silver Surfer",
-            answer: 2,
-        },
-
-
-        {
-            question: "2365214",
-            choice1: "Howard Stark",
-            choice2: "Batman",
-            choice3: "Captain America",
-            choice4: "Hulk",
-            answer: 3,
-        },
-
-
-        {
-            question: "2365214",
-            choice1: "Silver Heart",
-            choice2: "Curve Reactor",
-            choice3: "The Force",
-            choice4: "Arc Reactor",
-            answer: 4,
-        },
-
-
-        {
-            question: "asdfaf",
-            choice1: "The 10 Rings",
-            choice2: "Nick Fury",
-            choice3: "The Wakondians",
-            choice4: "Iron Mongler",
+            question: "What specific part of New York City is Steve Rogers from?",
+            choice1: "Brooklyn",
+            choice2: "Manhattan",
+            choice3: "Staten Island",
+            choice4: "Central Park",
             answer: 1,
         },
 
 
         {
-            question: "asdfaf",
-            choice1: "Plane Crash",
-            choice2: "Sailing",
-            choice3: "Car Crash",
-            choice4: "Storm",
+            question: "What symbol is on Captain America's shield?",
+            choice1: "Star",
+            choice2: "Strips",
+            choice3: "Dollar",
+            choice4: "Squares",
+            answer: 1,
+        },
+
+
+        {
+            question: "In Avengers: Civil War, who does Captain America fight?",
+            choice1: "Hulk",
+            choice2: "Iron Man",
+            choice3: "Vision",
+            choice4: "Jarvis",
+            answer: 2,
+        },
+
+
+        {
+            question:"What is Captain America's shield made out of?",
+            choice1: "Alloy Metal",
+            choice2: "Titanium alloy",
+            choice3: "Vibranium",
+            choice4: "Wood",
             answer: 3,
+        },
+
+
+        {
+            question:"Who is Captain America's best friend?",
+            choice1: "Bucky",
+            choice2: "Iron Man",
+            choice3: "Thor",
+            choice4: "Vision",
+            answer: 1,
+        },
+
+
+        {
+            question: "Who made Captain America’s shield?",
+            choice1: "Hydra",
+            choice2: "Howard Stark",
+            choice3: "Tony Stark",
+            choice4: "Asgard",
+            answer: 3,
+        },
+
+
+        {
+            question: "What year was Steve Rogers born?",
+            choice1: "1920",
+            choice2: "1917",
+            choice3: "1915",
+            choice4: "1918",
+            answer: 4,
+        },
+
+
+        {
+            question: "What does Steve Rogers hate above all else by World War II?",
+            choice1: "Man Kind",
+            choice2: "Bullies",
+            choice3: "Hydra",
+            choice4: "War",
+            answer: 2,
+        },
+
+
+        {
+            question: "What is the Super Soldier Project officially known as?",
+            choice1: "Super Soldier",
+            choice2: "Project Rebirth",
+            choice3: "Project Insight",
+            choice4: "Project America",
+            answer: 2,
+        },
+
+        {
+            question: "What was Captain America's elite unit known as?",
+            choice1: "Super Soldiers",
+            choice2: "Howling Commandos",
+            choice3: "Stars and Strips",
+            choice4: "Team America",
+            answer: 2,
+        },
+
+        {
+            question: "What is Captain Americas love interest",
+            choice1: "Peggy Carter",
+            choice2: "Peggy Walsh",
+            choice3: "Peggy Haris",
+            choice4: "Peggy Wilson",
+            answer: 1,
+        },
+
+        {
+            question: "What does Steve promise Peggy before he gets frozen?",
+            choice1: "Dancing",
+            choice2: "Dinner",
+            choice3: "Marriage",
+            choice4: "Drive",
+            answer: 1,
+        },
+
+        {
+            question: "What year was Captain America unfrozen?",
+            choice1: "2011",
+            choice2: "2014",
+            choice3: "2016",
+            choice4: "2008",
+            answer: 1,
+        },
+
+        {
+            question: "Finish this Captain America quote: 'This isn't freedom.'",
+            choice1: "This is America",
+            choice2: "This is Robbery",
+            choice3: "This is Fear",
+            choice4: "This is Communism",
+            answer: 3,
+        },
+
+        {
+            question: "What was Captain America able to pick up in Endgame?",
+            choice1: "Thanos",
+            choice2: "A Bus",
+            choice3: "A Car",
+            choice4: "Thors Hammer",
+            answer: 2,
+        },
+
+        {
+            question: "Where Does Captain America crash the plane belong to Red Skull",
+            choice1: "The Ocean",
+            choice2: "The Artic",
+            choice3: "The Desert",
+            choice4: "Space",
+            answer: 2,
+        },
+
+        {
+            question: "It turns out that there is a secret Hydra base in which of these locations?",
+            choice1: "The Mall of America",
+            choice2: "Coney Island Boardwalk",
+            choice3: "The White House",
+            choice4: "Steve's old training base",
+            answer: 4,
         },
 
 
--- assets/js/thor.js
+++ assets/js/thor.js
@@ -89,33 +89,110 @@ const thorQuiz = [
 
 
         {
-            question: "asdfaf",
-            choice1: "Silver Heart",
-            choice2: "Curve Reactor",
-            choice3: "The Force",
-            choice4: "Arc Reactor",
-            answer: 4,
-        },
-
-
-        {
-            question: "asdfaf",
-            choice1: "The 10 Rings",
-            choice2: "Nick Fury",
-            choice3: "The Wakondians",
-            choice4: "Iron Mongler",
+            question: "Which Actor Plays Thor?",
+            choice1: "Chris Hemsworth",
+            choice2: "Noel King",
+            choice3: "Chris Carter",
+            choice4: "Tony Howard",
             answer: 1,
         },
 
 
         {
-            question: "asdfaf",
-            choice1: "Plane Crash",
-            choice2: "Sailing",
-            choice3: "Car Crash",
-            choice4: "Storm",
+            question: "Which Villan destroyed Asgaurd",
+            choice1: "Surtur",
+            choice2: "Satan",
+            choice3: "Smog",
+            choice4: "Ice King",
+            answer: 1,
+        },
+
+
+        {
+            question: "What Nickname Does Stark Give To Thor?",
+            choice1: "Hammer Head",
+            choice2: "Point Break",
+            choice3: "Lazy Viking",
+            choice4: "Lightening Face",
+            answer: 2,
+        },
+
+        {
+            question: "Who Controls The Bifrost?",
+            choice1: "Balder",
+            choice2: "Vidar",
+            choice3: "Heimdall",
+            choice4: "Loki",
             answer: 3,
         },
+
+        {
+            question: "What Does Thor Call Rocket?",
+            choice1: "Rabbit",
+            choice2: "Squirrel",
+            choice3: "Chipmonk",
+            choice4: "Fox",
+            answer: 1,
+        },
+
+        {
+            question: "Who Is The Leader Of The Frost Giants?",
+            choice1: "Loki",
+            choice2: "Hail Strum",
+            choice3: "Raze",
+            choice4: "Laufey",
+            answer: 4,
+        },
+
+        {
+            question: "Which character is Thor's half brother?",
+            choice1: "Laufey",
+            choice2: "Heimdall",
+            choice3: "Loki",
+            choice4: "Balder",
+            answer: 3,
+        },
+
+        {
+            question: "What is the Asgardian name for the Earthly plane?",
+            choice1: "Hela",
+            choice2: "Midgard",
+            choice3: "Valhalla",
+            choice4: "Ragnorak",
+            answer: 2,
+        },
+
+        {
+            question: "What is the Asgardian world tree called?",
+            choice1: "Hela",
+            choice2: "Tree of Life",
+            choice3: "Yggdrasil",
+            choice4: "Hela",
+            answer: 3,
+        },
+
+        {
+            question: "What animals pull Thor's chariot?",
+            choice1: "Rabbits",
+            choice2: "Goats",
+            choice3: "Bulls",
+            choice4: "Pigs",
+            answer: 2,
+        },
+
+
+        {
+            question: "Thor was a founding member of what superhero team?",
+            choice1: "Hela",
+            choice2: "Time Keepers",
+            choice3: "Gaurdians",
+            choice4: "Avengers",
+            answer: 4,
+        },
+
+
+
+
 
 
  
